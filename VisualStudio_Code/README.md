b65e0b7d9fc8a429d81b90b69b7415a0985b4ed2
gopher194 <45499891+gopher194@users.noreply.github.com>
2021-10-13 12:50:25 -0400

    VSCode Settings

---
+++ VisualStudio_Code/README.md
@@ -0,0 +1,31 @@
+# My Visual Studio Code settings
+
+These few files are capturing my Visual Studio Code settings.
+They help a lot to code, manage repo, interact with Azure, develop PowerShell and bash scripts, Azure Pipelines, Kubernetes YAML, Terraform plans, etc. on Windows 11 (but works with Windows 10 also).
+
+## Installation steps
+
+1. Install Visual Studio Code
+
+   Installation comes in few flavors. They all start from here: <https://code.visualstudio.com/Download>
+
+2. Launch a cmd/Command Prompt
+
+   Copy/Paste the commands that installs the extensions you want to get in your context, from the file named `VSCode-Extensions.cmd`.
+   It is not formatted as a batch/bat script that can be run directly. I recommend starting with Copy/Paste to see how it gradually changes your Visual Studio Code environment.
+
+3. Edit Visual Studio Code user' settings
+
+   VS Code stores your user settings by default in this location: `C:\Users\<your user name>\AppData\Roaming\Code\User\settings.json`.
+   Open this file and edit it adding the settings from the `VSCode-settings.json` file.
+   The VS Code settings UI can also be used `File > Preferences > Settings`.
+
+4. Optional: Install 'Fire Mono' font
+
+   In the settings file, to get a beautiful look with the **Atom One Dark Theme**, I install the **Fira Mono** font and reference it in the `"editor.fontFamily"` setting.
+
+   To install the Font, visit the [Fira](https://github.com/mozilla/Fira) github repo and download the TTF version of the Regular font, which is here: <https://github.com/mozilla/Fira/blob/master/ttf/FiraMono-Regular.ttf>
+
+   Once downloaded, double-click on it, it opens the Font display windows, Font name being "Fira Mono". Click on "Install".
+
+   I also added the file in this directory.
---
+++ VisualStudio_Code/VSCode-Extensions.cmd
@@ -0,0 +1,147 @@
+# From CMD (Command Prompt, located here: %windir%\system32\cmd.exe) run the following:
+
+# Get indentation rainbow:
+code --install-extension oderwat.indent-rainbow
+
+# Customize Explorer Icons
+code --install-extension vscode-icons-team.vscode-icons
+
+# Terraform related
+code --install-extension 4ops.terraform
+code --install-extension hashicorp.terraform
+
+# GitLens Expansion
+code --install-extension eamodio.gitlens
+
+# Atom One Dark Theme
+code --install-extension akamud.vscode-theme-onedark
+# Usually used with Fira Font: https://github.com/mozilla/Fira
+# Open and Install Fira Mono from the TTF folder in Fira repo
+# Add this to the settings.json:
+#  "editor.fontFamily": "'Fira Mono', Menlo, Monaco, 'Courier New', monospace"
+
+# YAML support by RedHat
+code --install-extension redhat.vscode-yaml
+
+# Prettier code Formatter: https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+code --install-extension esbenp.prettier-vscode
+# Formatting Toggler
+code --install-extension tombonnike.vscode-status-bar-format-toggle
+
+# Azure related extensions
+
+## Azure Pipelines
+code --install-extension ms-azure-devops.azure-pipelines
+# Note: add this to settings.json
+#"[azure-pipelines]": {
+#  "editor.defaultFormatter": "esbenp.prettier-vscode"
+#},
+# Docker
+code --install-extension ms-azuretools.vscode-docker
+# C#
+code --install-extension ms-dotnettools.csharp
+# Kubernetes
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+# Remote WSL
+code --install-extension ms-vscode-remote.remote-wsl
+# PowerShell
+code --install-extension ms-vscode.powershell
+# Sign In/Out of Azure
+code --install-extension ms-vscode.azure-account
+# Azure CloudFormation
+code --install-extension ms-vscode.azurecli
+# Azure DevOps Wiki Markdown
+code --install-extension ni.azdo-wiki-md
+
+#code --install-extension ms-azuretools.vscode-azureappservice
+#code --install-extension ms-azuretools.vscode-azurefunctions
+#code --install-extension ms-azuretools.vscode-cosmosdb
+#code --install-extension ms-vsts.team
+
+# Code spellchecker (cSpell)
+code --install-extension streetsidesoftware.code-spell-checker
+
+# Markdown Lint
+code --install-extension DavidAnson.vscode-markdownlint
+# Markdown shortcuts
+code --install-extension mdickin.markdown-shortcuts
+
+
+#####################################  End of Installed  #####################################
+# JavaScript formatter
+#code --install-extension dai-shi.vscode-es-beautifier
+
+# Angular
+#code --install-extension 1tontech.angular-material
+#code --install-extension Angular.ng-template
+#code --install-extension johnpapa.Angular2
+
+# Project Manager
+#code --install-extension alefragnani.project-manager
+
+# One Monokai Theme: https://marketplace.visualstudio.com/items?itemName=azemoh.one-monokai
+#code --install-extension azemoh.one-monokai
+
+# Shell: lets you execute shell commands directly from the editor Palette.
+#code --install-extension bbenoist.shell
+
+# ESLint: Find and Fix problems in JavaScript code
+#code --install-extension dbaeumer.vscode-eslint
+
+# AWS CloudFormation snippet
+#code --install-extension dsteenman.cloudformation-yaml-snippets
+
+###  Themes  ###
+# Community Material Theme
+#code --install-extension Equinusocio.vsc-community-material-theme
+# Material Theme
+#code --install-extension Equinusocio.vsc-material-theme
+#code --install-extension equinusocio.vsc-material-theme-icons
+
+# FlatUI theme
+#code --install-extension lkytal.FlatUI
+
+# Other themes
+#code --install-extension PKief.material-icon-theme
+#code --install-extension SolarLiner.linux-themes
+#code --install-extension thenikso.github-plus-theme
+#code --install-extension tinkertrain.theme-panda
+#code --install-extension zhuangtongfa.material-theme
+#code --install-extension whizkydee.material-palenight-theme
+###  End of Themes  ###
+
+# Ionic Snippets for VS Code
+#code --install-extension fivethree.vscode-ionic-snippets
+#code --install-extension ionic-preview.ionic-preview
+
+# Code look
+#code --install-extension HookyQR.beautify
+#code --install-extension mblode.pretty-formatter
+
+# C# Extensions
+#code --install-extension jchannon.csharpextensions
+
+# CloudFoundry Manifest YAML
+#code --install-extension Pivotal.vscode-manifest-yaml
+
+# Node.js intellisense
+#code --install-extension leizongmin.node-module-intellisense
+
+# Terraform snippets
+#code --install-extension mindginative.terraform-snippets
+# Terraform Documentation snippet
+#code --install-extension run-at-scale.terraform-doc-snippets
+# Azure Terraform to test and deploy in Azure
+#code --install-extension ms-azuretools.vscode-azureterraform
+
+# Python
+#code --install-extension ms-python.python
+
+# Debugger for Microsoft Edge
+#code --install-extension msjsdiag.debugger-for-edge
+
+# Jinja syntax highlighter
+#code --install-extension samuelcolvin.jinjahtml
+
+# Yseop Markup Language
+#code --install-extension Yseop.vscode-yseopml
